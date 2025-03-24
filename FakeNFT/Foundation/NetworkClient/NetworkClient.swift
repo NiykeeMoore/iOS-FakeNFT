3bcbeeca4246d1680a06238386ed38efce2918f7
Vladimir Golovin <cocacolalocal@ya.ru>
2025-03-25 00:14:41 +0300

    SwiftLint added and fixed a warnings

--- FakeNFT/Foundation/NetworkClient/NetworkClient.swift
+++ FakeNFT/Foundation/NetworkClient/NetworkClient.swift
@@ -60,7 +60,9 @@ struct DefaultNetworkClient: NetworkClient {
                 onResponse(result)
             }
         }
-        guard let urlRequest = create(request: request) else { return nil }
+        guard let urlRequest = create(request: request) else {
+            return nil
+        }
 
         let task = session.dataTask(with: urlRequest) { data, response, error in
             guard let response = response as? HTTPURLResponse else {
